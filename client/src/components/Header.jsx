3349f73f6c683d955bb2f6cdda3a8f47f682ad68
Adnan-0001 <adnan.naseer@devsinc.com>
2023-10-31 02:03:48 +0500

    Add header and protectedComponent

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,34 @@
 import { NavLink } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectCurrentUserId,
+  selectCurrentRefreshToken,
+} from "../features/auth/authSlice";
+import { useLogoutMutation } from "../features/auth/authApiSlice";
+import { clearCredentials } from "../features/auth/authSlice";
 
 export const Header = () => {
+  const currUserId = useSelector(selectCurrentUserId);
+  const curRefreshToken = useSelector(selectCurrentRefreshToken);
+
+  const dispatch = useDispatch();
+
+  const [logout] = useLogoutMutation();
+  const onLogout = async () => {
+    try {
+      // const tokens = localStorage.getItem("authTokens")
+      //   ? JSON.parse(localStorage.getItem("authTokens"))
+      //   : null;
+      // const refreshToken = tokens?.refresh;
+      const result = await logout(curRefreshToken).unwrap();
+      dispatch(clearCredentials());
+
+      navigate("/login");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <nav className="navbar">
       <NavLink
@@ -9,13 +37,23 @@ export const Header = () => {
       >
         Home
       </NavLink>
-      <span> </span>
+
       <NavLink
-        to="/login"
+        to="/dashboard"
         className={({ isActive }) => (isActive ? "link active" : "link")}
       >
-        Login
+        Dashboard
       </NavLink>
+      {currUserId ? (
+        <a onClick={onLogout}>Logout</a>
+      ) : (
+        <NavLink
+          to="/login"
+          className={({ isActive }) => (isActive ? "link active" : "link")}
+        >
+          Login
+        </NavLink>
+      )}
     </nav>
   );
 };
--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,13 +1,18 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectCurrentUserId } from "../features/auth/authSlice";
+import { useEffect } from "react";
 
 const ProtectedRoute = ({ children }) => {
   const userId = useSelector(selectCurrentUserId);
 
-  if (!userId) {
-    return <Navigate to="/" />;
+  console.log("in");
+  const tokens = localStorage.getItem("authTokens");
+  if (!tokens) {
+    return <Navigate to="/login" replace={true} />;
   }
+  console.log("bypassing if", children);
+  // return <Outlet />;
   return children;
 };
 export default ProtectedRoute;
